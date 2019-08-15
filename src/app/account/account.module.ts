04ffc3df3258ec27e978ec4dca119561a2446597
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-08-15 20:08:04 +0200

    account page holder added

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountComponent } from './account.component';
 import { AccountRoutingModule } from './account.routing.module';
+import {AccountHeaderComponent} from "./header/header.component";
 
 
 @NgModule({
-  declarations: [AccountComponent],
+  declarations: [AccountComponent, AccountHeaderComponent],
   imports: [
     CommonModule,
     AccountRoutingModule
---
+++ src/app/account/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'acc-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass']
+})
+export class AccountHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
