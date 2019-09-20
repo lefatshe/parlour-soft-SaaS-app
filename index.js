abef1ff79c4f31d1ff11a7cc8020541f12a5c98f
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-09-20 21:54:27 +0200

    save new look styles

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const app = express();
+const appName = 'ParlourSoft';
+const port = process.env.PORT || 5000
 
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Serve static files....
+app.use(express.static(__dirname + '/dist/' + appName));
 
+// Send all requests to index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ParlourSoft/index.html'));
+});
 
-  const express = require('express');
-  const path = require('path');
-  const app = express();
-  const appName = 'ParlourSoft';
-  const port = process.env.PORT || 5000
-  
-  // Serve static files....
-  app.use(express.static(__dirname + '/dist/' + appName));
-  
-  // Send all requests to index.html
-  app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ParlourSoft/index.html'));
-  });
-  
-  // default Heroku PORT
-  app.listen(port);
-  
+// default Heroku PORT
+app.listen(port);
+
 
--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AccountComponent } from './account.component';
-import { AccountRoutingModule } from './account.routing.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AccountComponent} from './account.component';
+import {AccountRoutingModule} from './account.routing.module';
 import {AccountHeaderComponent} from "./header/header.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 
 @NgModule({
-  declarations: [AccountComponent, AccountHeaderComponent],
+  declarations: [AccountComponent, AccountHeaderComponent, DashboardComponent],
   imports: [
     CommonModule,
     AccountRoutingModule
   ]
 })
-export class AccountModule { }
+export class AccountModule {
+}
